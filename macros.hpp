6a58285206ba281927a48e639bbdd12c3547db88
Robert JK Smith <robertjksmith@live.co.uk>
2020-07-09 09:18:43 +0100

    Added macros.

---
+++ macros.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+// Remove params
+#define EAT(...)
+
+// Seperate params
+#define EXPAND(...) __VA_ARGS__
+
+// Combine params
+#define CONCAT_1(a, b) a ## b
+#define CONCAT(a, b) CONCAT_1(a, b)
+
+// Make into string
+#define STRINGIFY(x) #x
+
+// Array length
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(*x))
+
+// Implementation of the defer macro
+namespace
+{
+	// Use destructor to Call captured function at end of scope (Scope guard)
+	template<typename Func> struct DeferImp
+	{
+		Func f;
+		~DeferImp(){f();}
+	};
+
+	// Dummy struct used for the next trick
+	struct DeferTemp{};
+
+	// Deduce correct template arguments for DeferImp. No brackets '()' needed
+	template<typename Func> DeferImp<Func> operator<<(DeferTemp, Func f)
+	{
+		return {f};
+	};
+}
+// We generate a uniquly named DeferImp object, concatinating the line number
+// Then deduce the DeferImp template parameters using operator <<
+// Finally the exposed lamda captures the outside scope and is used as the block of code to be deferred
+
+// Call block of code at the end of scope
+#define DEFER_TO_SCOPE auto CONCAT(zz_defer_imp, __LINE__) = DeferTemp{} << [&]()
