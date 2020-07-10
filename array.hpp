37962dca3fcd18b06d9d78fe70efd776b5a6470e
Robert JK Smith <robertjksmith@live.co.uk>
2020-07-10 01:35:49 +0100

    Ranged arrayptrs. Create arraypts from carrays. Deduce the size of arrays.

--- array.hpp
+++ array.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "macros.hpp"
+
+// size_t
 #include <cstddef>
-#include <cstring>
 
 // Holds a ponter to a array and its size
 template<typename T> struct ArrayPtr
@@ -13,9 +15,18 @@ template<typename T> struct ArrayPtr
 	      T& operator[](size_t i)       {return data[i];};
 	const T& operator[](size_t i) const {return data[i];}
 
+	// Used by range for loop
+	      T* begin()       {return data;}
+	const T* begin() const {return data;}
+	      T* end()         {return data + size;}
+	const T* end()   const {return data + size;}
+
 	// Implicit conversion to const
 	operator ArrayPtr<const T>() const {return {data, size};}
 };
+
+// Creates an ArrayPtr from a c array
+#define ARRAY_PTR(x) ArrayPtr<std::remove_all_extents<decltype(x)>::type>{x, ARRAY_SIZE(x)}
 
 // Wrapper around an array that knows its own size
 template<typename T, size_t N> struct Array
@@ -27,11 +38,13 @@ template<typename T, size_t N> struct Array
 	Array()                        = default;
 	~Array()                       = default;
 
-	// Disable copy / move construction
+	// Allow move construction
+	Array(Array&&)                 = default;
+	Array& operator=(Array&&)      = default;
+
+	// Disable copy construction
 	Array(const Array&)            = delete;
-	Array(Array&&)                 = delete;
 	Array& operator=(const Array&) = delete;
-	Array& operator=(Array&&)      = delete;
 
 	// Array access
 	      T& operator[](size_t i)       {return data[i];}
@@ -46,3 +59,8 @@ template<typename T, size_t N> struct Array
 	inline ArrayPtr<const T> ptr() const{return {data, size};}
 };
 
+// Deduce the size of the array from input params
+template<typename T, typename... TS> Array<T, sizeof...(TS)> make_array(TS&&... t)
+{
+	return {t...};
+}
