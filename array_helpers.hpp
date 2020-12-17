b2c60466c48aa05277d0cbc8168235c237fc6c4e
Robert JK Smith <robertjksmith@live.co.uk>
2020-12-17 00:25:48 +0000

    Merged in changes from pwarb.

---
+++ array_helpers.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "array_ptr.hpp"
+#include "array.hpp"
+#include <type_traits>
+#include <cstring>
+
+template<typename T> T& get_first(ArrayPtr<T> ptr)
+{
+	return *ptr.begin();
+}
+
+template<typename T> T& get_last(ArrayPtr<T> ptr)
+{
+	return *(ptr.end() - 1);
+}
+
+template<typename T> T* linear_search(ArrayPtr<T> ptr, const T& value)
+{
+	for(size_t i = 0; i < ptr.size(); ++i)
+	{
+		if(ptr[i] == value)
+		{
+			return &ptr[i];
+		}
+	}
+	return nullptr;
+}
+
+template<typename T> inline size_t byte_size(ArrayPtr<T> ptr)
+{
+	return ptr.size() * sizeof(T);
+}
+
+template<typename T> ArrayPtr<T> to_ptr(ArrayPtr<T> ptr)
+{
+    return ptr;
+}
+
+template<typename T> ArrayPtr<T> to_ptr(T* ptr, size_t n)
+{
+	return {ptr, n};
+}
+
+template<typename T, size_t N> ArrayPtr<T> to_ptr(T (&arr)[N])
+{
+    return {arr};
+}
+
+template<typename T> ArrayPtr<typename T::value_type> to_ptr(T& t)
+{
+    return {t};
+}
+
+template<typename T> ArrayPtr<const typename T::value_type> to_ptr(const T& t)
+{
+    return {t};
+}
+
+template<typename T, size_t N> ArrayPtr<T> to_ptr(Array<T, N>& arr)
+{
+	return {arr.data(), arr.size()};
+}
+
+template<typename T, size_t N> ArrayPtr<const T> to_ptr(const Array<T, N>& arr)
+{
+	return {arr.data(), arr.size()};
+}
+
+template<typename T> inline void zero(ArrayPtr<T> ptr, int val = 0)
+{
+	static_assert(!std::is_const<T>(), "Cannot zero const array");
+	memset(ptr.data(), val, byte_size(ptr));
+}
+
+template<typename T, typename U> inline void copy(ArrayPtr<T> dst, ArrayPtr<U> src)
+{
+	static_assert(!std::is_const<T>(),  "Cannot copy to const array");
+	memcpy(dst.data(), src.data(), byte_size(src));
+}
--- array_ptr.hpp
+++ array_ptr.hpp
@@ -1,18 +1,16 @@
 #pragma once
 
-#include <cstdint>
-#include <cstddef>
-#include <cstring>
-#include <type_traits>
+#include <cstddef> // For size_t
 
 template<typename T> class ArrayPtr
 {
 public:
 	inline ArrayPtr();
-	inline ArrayPtr(std::nullptr_t);
+	inline ArrayPtr(nullptr_t);
 	inline ArrayPtr(T* t, size_t n);
 	template<size_t N>
 	inline ArrayPtr(T(&t)[N]);
+
 	inline           operator ArrayPtr<const T>() const;
 	inline           operator bool()        const;
 
@@ -31,26 +29,27 @@ private:
 	size_t data_size;
 };
 
-template<typename T> ArrayPtr<T>::ArrayPtr(std::nullptr_t)
-	: data_ptr{nullptr}, data_size{0}
+template<typename T> ArrayPtr<T>::ArrayPtr() :
+	data_ptr{nullptr},
+	data_size{0}
 {
 }
 
-template<typename T> ArrayPtr<T>::ArrayPtr(T* t, size_t n)
-	: data_ptr{t}, data_size{n}
+template<typename T> ArrayPtr<T>::ArrayPtr(nullptr_t) :
+	ArrayPtr{}
 {
 }
 
-template<typename T> ArrayPtr<T>::ArrayPtr()
-	: ArrayPtr{nullptr}
+template<typename T> ArrayPtr<T>::ArrayPtr(T* t, size_t n) :
+	data_ptr{t},
+	data_size{n}
 {
 }
 
-template<typename T> template<size_t N> inline ArrayPtr<T>::ArrayPtr(T(&t)[N])
-	: ArrayPtr{t, N}
+template<typename T> template<size_t N> inline ArrayPtr<T>::ArrayPtr(T(&t)[N]) :
+	ArrayPtr{t, N}
 {
 }
-
 
 template<typename T> ArrayPtr<T>::operator ArrayPtr<const T>() const
 {
@@ -72,7 +71,7 @@ template<typename T> T* ArrayPtr<T>::begin()
 	return data_ptr;
 }
 
-template<typename T> const T* ArrayPtr<T>::begin() const 
+template<typename T> const T* ArrayPtr<T>::begin() const
 {
 	return data_ptr;
 }
@@ -110,46 +109,4 @@ template<typename T> size_t& ArrayPtr<T>::size()
 template<typename T> size_t  ArrayPtr<T>::size() const
 {
 	return data_size;
-}
-
-template<typename T> inline size_t byte_size(ArrayPtr<T> ptr)
-{
-	return ptr.size() * sizeof(T);
-}
-
-template<typename T> ArrayPtr<T> to_ptr(ArrayPtr<T> ptr)
-{
-    return ptr;
-}
-
-template<typename T> ArrayPtr<T> to_ptr(T* ptr, size_t n)
-{
-	return {ptr, n};
-}
-
-template<typename T, size_t N> ArrayPtr<T> to_ptr(T (&arr)[N])
-{
-    return {arr};
-}
-
-template<typename T> ArrayPtr<typename T::value_type> to_ptr(T& t)
-{
-    return {t};
-}
-
-template<typename T> ArrayPtr<const typename T::value_type> to_ptr(const T& t)
-{
-    return {t};
-}
-
-template<typename T> inline void zero(ArrayPtr<T> ptr, int val = 0)
-{
-	static_assert(!std::is_const<T>(), "Cannot zero const array");
-	memset(ptr.data(), val, byte_size(ptr));
-}
-
-template<typename T, typename U> inline void copy(ArrayPtr<T> dst, ArrayPtr<U> src)
-{
-	static_assert(!std::is_const<T>(),  "Cannot copy to const array");
-	memcpy(dst.data(), src.data(), byte_size(src));
 }
