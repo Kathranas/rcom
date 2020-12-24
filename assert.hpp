bd3dee65acb45822deff2d4d9425f212d3cee46f
Robert JK Smith <robertjksmith@live.co.uk>
2020-12-24 20:50:37 +0000

    Simplified assert_implementation.

--- assert.hpp
+++ assert.hpp
@@ -7,73 +7,28 @@
 
 namespace rcom
 {
-	class AssertHandler
+#if defined(RCOM_OVERRIDE_ASSERTS)
+	// Provide your own assert implementation
+	extern void assert_imp(const char* fmt, ...);
+#else
+	// Use default
+	inline void assert_imp(const char* fmt, ...)
 	{
-	public:
-		virtual void run(const char* fmt, ...) = 0;
-	};
-	
-	class DefaultAssertHandlerImp : public AssertHandler
-	{
-	public:
-		inline virtual void run(const char* fmt, ...) override
-		{
-			va_list argp;
-			va_start(argp, fmt);
-			vfprintf(stderr, fmt, argp);
-			va_end(argp);
-			fputc('\n', stderr);
-			abort();
-		}
-	};
-	
-	// Singleton for getting / setting assert handlers
-	class AssertTable
-	{
-	public:
-		AssertTable(const AssertTable&)            = delete;
-		AssertTable(AssertTable&&)                 = delete;
-		AssertTable& operator=(const AssertTable&) = delete;
-		AssertTable& operator=(AssertTable&&)      = delete;
-	
-		inline static AssertHandler* get_default()
-		{
-			return get().default_handler;
-		}
-		inline static void set_default(AssertHandler& handler)
-		{
-			get().default_handler = &handler;
-		}
-		inline static void reset_default()
-		{
-			auto& self = get();
-			self.default_handler = &self.default_imp;
-		}
-	private:
-		DefaultAssertHandlerImp default_imp;
-		AssertHandler* default_handler;
-	
-		AssertTable() :
-			default_imp{},
-			default_handler{}
-		{
-			default_handler = &default_imp;
-		}
-	
-		inline static AssertTable& get()
-		{
-			static AssertTable inst{};
-			return inst;
-		}
-	};
-
+		va_list argp;
+		va_start(argp, fmt);
+		vfprintf(stderr, fmt, argp);
+		va_end(argp);
+		fputc('\n', stderr);
+		abort();
+	}
+#endif
 }
-// namespace::rcom
+// namespace RCOM
 
 #define RCOM_ASSERT_MSG(msg) "Failed Assertion: " msg " at: " RCOM_CODE_LOCATION
 
 #if defined(RCOM_ASSERTS_ENABLED)
-	#define RCOM_ASSERT(x, msg, ...) (x) ? ;rcom::AssertAssigner::get_default()->run(RCOM_ASSERT_MSG(msg), __VA_ARGS__)
+	#define RCOM_ASSERT(x, msg, ...) if(x) rcom::assert_imp(RCOM_ASSERT_MSG(msg), __VA_ARGS__)
 #else
 	#define RCOM_ASSERT(x, msg, ...)
 #endif
