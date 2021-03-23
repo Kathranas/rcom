8025d0f544c5ba3ece574ea86ce76be40a3ff5a2
Robert JK Smith <robertjksmith@live.co.uk>
2021-03-23 22:16:22 +0000

    General array improvements.

--- array.hpp
+++ array.hpp
@@ -5,6 +5,11 @@
 #include "array_ptr.hpp"
 #include <initializer_list>
 
+#define RCOM_ARRAY_SIZE(ARR) (sizeof(ARR) / sizeof(*ARR))
+
+#define RCOM_MAKE_ARRAY(NAME, TYPE, ...)  rcom::Array<TYPE, std::initializer_list<TYPE>__VA_ARGS__.size()> NAME{__VA_ARGS__}
+#define RCOM_FROM_STRING(NAME, TYPE, ARR) rcom::Array<TYPE, RCOM_ARRAY_SIZE(ARR)> NAME{ARR}
+
 namespace rcom
 {
 	// Multidimensional array
@@ -15,10 +20,11 @@ namespace rcom
 	public:
 		// Required to be an aggrigate
 		T _arr[N]; // Do not access this directly. Use data() instead
+
+		Array()                        = default;
 		Array(const Array&)            = default;
 		Array(Array&&)                 = default;
 
-		Array()                        = default;
 		Array& operator=(const Array&) = default;
 		Array& operator=(Array&&)      = default;
 		~Array()                       = default;
@@ -26,18 +32,14 @@ namespace rcom
 		inline static constexpr size_t size();
 		inline static constexpr size_t byte_size();
 
-		// Multidimensional
-		inline static constexpr size_t flat_size();
-		inline static constexpr size_t sub_array_count();
-
 		inline       ArrayPtr<T> to_ptr();
 		inline const ArrayPtr<T> to_ptr() const;
 
 		inline       BytePtr to_bytes();
 		inline const BytePtr to_bytes() const;
 
-		inline       T& operator[](size_t i);
-		inline const T& operator[](size_t i) const;
+		inline constexpr      T& operator[](size_t i);
+		inline constexpr const T& operator[](size_t i) const;
 
 		inline       T* data();
 		inline const T* data()   const;
@@ -50,9 +52,6 @@ namespace rcom
 		inline       T& last();
 		inline const T& last()   const;
 	};
-
-#define RCOM_MAKE_ARRAY(NAME, TYPE, ...)  rcom::Array<TYPE, std::initializer_list<TYPE>__VA_ARGS__.size()> NAME{__VA_ARGS__}
-#define RCOM_FROM_STRING(NAME, TYPE, ARR) rcom::Array<TYPE, (sizeof(ARR) / sizeof(*ARR))> NAME{ARR}
 
 	template<typename T, size_t N> ArrayPtr<T> Array<T, N>::to_ptr()
 	{
@@ -79,13 +78,13 @@ namespace rcom
 		return {static_cast<uint8_t*>((void*)data()), byte_size()};
 	}
 
-	template<typename T, size_t N> T& Array<T, N>::operator[](size_t i)
+	template<typename T, size_t N> constexpr T& Array<T, N>::operator[](size_t i)
 	{
 		RCOM_ASSERT(i < size(), "Index out of range");
 		return _arr[i];
 	}
 	
-	template<typename T, size_t N> const T& Array<T, N>::operator[](size_t i) const
+	template<typename T, size_t N> constexpr const T& Array<T, N>::operator[](size_t i) const
 	{
 		RCOM_ASSERT(i < size(), "Index out of range");
 		return _arr[i];
@@ -146,11 +145,6 @@ namespace rcom
 		return N;
 	}
 
-	template<typename T, size_t N> constexpr size_t Array<T, N>::flat_size()
-	{
-		return size();
-	}
-
 	// Multidimensional array
 	template<typename T, size_t N, size_t... NS> class Array
 	{
@@ -158,7 +152,13 @@ namespace rcom
 		typedef Array<T, NS...> ArrayType;
 
 		// Required to be an aggrigate
-		Array<ArrayType, N> _arr;
+		// Use C array instead of Array due to indexing
+		ArrayType _arr[N];
+
+		// Sizes of each array available at compile time
+		constexpr static const Array<size_t, 1 + sizeof...(NS)> _sizes{N, NS...};
+
+		Array()                        = default;
 		Array(const Array&)            = default;
 		Array(Array&&)                 = default;
 
@@ -166,20 +166,18 @@ namespace rcom
 		Array& operator=(Array&&)      = default;
 		~Array()                       = default;
 
+		// Multidimensional
+		template<size_t NN = 0>
 		inline static constexpr size_t size();
 		inline static constexpr size_t byte_size();
-
-		// Multidimensional
-		inline static constexpr size_t flat_size();
-		inline static constexpr size_t sub_array_count();
 
 		inline       ArrayPtr<ArrayType> to_ptr();
 		inline const ArrayPtr<ArrayType> to_ptr() const;
 		inline       BytePtr             to_bytes();
 		inline const BytePtr             to_bytes() const;
 
-		inline       ArrayType& operator[](size_t i);
-		inline const ArrayType& operator[](size_t i)  const;
+		inline constexpr       ArrayType& operator[](size_t i);
+		inline constexpr const ArrayType& operator[](size_t i)  const;
 
 		inline       ArrayType* data();
 		inline const ArrayType* data()   const;
@@ -195,92 +193,83 @@ namespace rcom
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::to_ptr() -> ArrayPtr<ArrayType>
 	{
-		return _arr.to_ptr();
+		return {_arr, N};
 	}
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::to_ptr() const -> const ArrayPtr<ArrayType>
 	{
-		return _arr.to_ptr();
+		return {_arr, N};
 	}
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::to_bytes() -> BytePtr
 	{
-		return _arr.to_bytes();
+		return {static_cast<uint8_t*>((void*)data()), byte_size()};
 	}
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::to_bytes() const -> const BytePtr
 	{
-		return _arr.to_bytes();
+		return {static_cast<uint8_t*>((void*)data()), byte_size()};
 	}
 
-	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::operator[](size_t i) -> ArrayType&
+	template<typename T, size_t N, size_t... NS> constexpr auto Array<T, N, NS...>::operator[](size_t i) -> ArrayType&
 	{
 		return _arr[i];
 	}
 
-	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::operator[](size_t i) const -> const ArrayType&
+	template<typename T, size_t N, size_t... NS> constexpr auto Array<T, N, NS...>::operator[](size_t i) const -> const ArrayType&
 	{
 		return _arr[i];
 	}
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::begin() -> ArrayType*
 	{
-		return _arr.begin();
+		return &_arr[0];
 	}
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::begin() const -> const ArrayType*
 	{
-		return _arr.begin();
+		return &_arr[0];
 	}
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::end() -> ArrayType*
 	{
-		return _arr.end();
+		return &_arr[N];
 	}
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::end() const -> const ArrayType*
 	{
-		return _arr.end();
+		return &_arr[N];
 	}
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::first() -> ArrayType&
 	{
-		return _arr.first();
+		return _arr[N];
 	}
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::first() const -> const ArrayType&
 	{
-		return _arr.first();
+		return _arr[N];
 	}
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::last() -> ArrayType&
 	{
-		return _arr.last();
+		return _arr[N-1];
 	}
 
 	template<typename T, size_t N, size_t... NS> auto Array<T, N, NS...>::last() const -> const ArrayType&
 	{
-		return _arr.last();
+		return _arr[N-1];
 	}
 
-	template<typename T, size_t N, size_t... NS> constexpr size_t Array<T, N, NS...>::size()
+	template<typename T, size_t N, size_t... NS>
+	template<size_t NN> constexpr size_t Array<T, N, NS...>::size()
 	{
-		return ArrayType::size();
+		return _sizes[NN];
 	}
 
 	template<typename T, size_t N, size_t... NS> constexpr size_t Array<T, N, NS...>::byte_size()
 	{
-		return N * sizeof(ArrayType);
-	}
-
-	template<typename T, size_t N, size_t... NS> constexpr size_t Array<T, N, NS...>::flat_size()
-	{
-		return N * ArrayType::flat_size();
-	}
-
-	template<typename T, size_t N, size_t... NS> constexpr size_t Array<T, N, NS...>::sub_array_count()
-	{
-		return sizeof...(NS);
+		return size() * sizeof(T);
 	}
 }
 // namespace::rcom
